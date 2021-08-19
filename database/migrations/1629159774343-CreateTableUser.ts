d2e1216ced31b1f67d57c7cb6fd266a9cf347ff3
Wendell Douglas <wendell.douglas@marketeasy.store>
2021-08-18 23:04:58 -0300

    configuration typeorm with migrations/entity

---
+++ database/migrations/1629159774343-CreateTableUser.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableUser1629159774343 implements MigrationInterface {
+  name = 'CreateTableUser1629159774343';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "isActive" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
+}
